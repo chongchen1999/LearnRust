1d4eb0c3f3ec4ccfd94004173e70783929f399d5
Chong Chen <chongchen1999@gmail.com>
2025-01-10 23:45:30 -0500

    ff

--- _02_guess_game/src/main.rs
+++ _02_guess_game/src/main.rs
@@ -1,5 +1,12 @@
+use rand::Rng; // trait
+
 fn main() {
     println!("Guess a number:");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("The secret number is: {}", secret_number);
+
     println!("Please input your guess.");
 
     let mut guess = String::new();
