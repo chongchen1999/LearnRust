29ef299fc563c56eb42a2de233dbc1b7c846eb99
Chong Chen <chongchen1999@gmail.com>
2025-01-11 11:39:30 -0500

    update

---
+++ 03-general_concepts/compound/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+    println!("{} {} {}", x, y, z);
+
+    let index = [12, 13, 14, 15];
+    let a: [i32; 15] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+    // Ensure that the index is within bounds
+    if index[2] < a.len() {
+        println!("{}", a[index[2]]);
+    } else {
+        println!("Index out of bounds");
+    }
+
+    another_function(5, 6);
+}
+
+fn another_function(x: i32, y: i32) {
+    println!("{} {}", x, y);
+}
