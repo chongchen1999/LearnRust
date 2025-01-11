815a8358c28f7eb2b4a37fa8a41321de64806b23
Chong Chen <chongchen1999@gmail.com>
2025-01-11 00:09:20 -0500

    update styles

---
+++ 02-guess_game/guess_game/src/main.rs
@@ -0,0 +1,33 @@
+use rand::Rng;
+
+fn main() {
+    println!("Wellcome to the guess number game!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        std::io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            std::cmp::Ordering::Less => println!("Too small!"),
+            std::cmp::Ordering::Greater => println!("Too big!"),
+            std::cmp::Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
