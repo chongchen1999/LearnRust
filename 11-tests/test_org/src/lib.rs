8bf37b51891bd27d5783157ef5c058880d246b19
Chong Chen <chongchen1999@gmail.com>
2025-01-13 20:57:56 -0500

    add tests basics

---
+++ 11-tests/test_org/src/lib.rs
@@ -0,0 +1,11 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+pub fn add_two(a: usize) -> usize {
+    internal_adder(a, 2)
+}
+
+fn internal_adder(left: usize, right: usize) -> usize {
+    left + right
+}
