a7002337ec8f55fdf3373bc5822f2568c67a562f
Chong Chen <chongchen1999@gmail.com>
2025-01-14 18:47:14 -0500

    update closure

---
+++ 13-iterators_and_closures/closure_/src/lib.rs
@@ -0,0 +1,2 @@
+pub mod simple_demo;
+pub mod capture_demo;
---
+++ 13-iterators_and_closures/closure_/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    closure_::simple_demo::run();
+    println!("{}","-".repeat(50));
+
+    closure_::capture_demo::immut_ref();
+}
