8d6c1734af0af4a38fc8ca4c9c5c87d1199b4c4f
Chong Chen <chongchen1999@gmail.com>
2025-01-20 15:06:13 -0500

    add pattern syntax

---
+++ 18-pattern_match/pattern_syntax/src/bin/ignoring_values.rs
@@ -0,0 +1,28 @@
+fn main() {
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, _, third, _, fifth) => {
+            println!("Some numbers: {first}, {third}, {fifth}")
+        }
+    }
+
+    let num = Some(4);
+
+    match num {
+        Some(x) if x % 2 == 0 => println!("The number {x} is even"),
+        Some(x) => println!("The number {x} is odd"),
+        None => (),
+    }
+
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(n) if n == y => println!("Matched, n = {n}"),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {}", x, y);
+}
